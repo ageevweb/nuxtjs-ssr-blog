f6df44782453bb70f67e5919d38efd787fbc8fe5
ageevweb <ageevweb@gmail.com>
2020-04-24 20:24:58 +0300

    home page + intro comp

---
+++ components/Promo.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  section.promo
+    .container
+      h1.title My SSR Blog on Nuxt.js
+      p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+</template>
+
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss">
+  .promo{
+    text-align: center;
+    
+    p{
+      color: #999;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
   .wrapper-content.wrapper-content--fixed
-  
+    promo
+    intro(title="My last posts: ")
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import promo from '~/components/Promo.vue'
+import intro from '~/components/UI/Intro.vue'
+
 
 export default {
   components: {
-    Logo
+    promo,
+    intro
   }
 }
 </script>
