766587bf0a57f83d0fcc228e9b55a9ba0f52ea45
ageevweb <ageevweb@gmail.com>
2020-04-24 17:42:50 +0300

    add header + scss conf + pug

---
+++ components/system/Header.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  header
+    .navbar
+      .container
+        .navbar-content
+          .logo Blog33
+          ul.navbar-list
+            li.navbar-item(v-for="link in links" :key="link.title")
+              nuxt-link.navbar-link(:title="link.title" :to="link.url") {{ link.title }}
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        links: [
+          { title: 'Home', url: '/' }
+        ]
+      }
+    }
+  }
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="">
+        <h1>POSTS</h1>
+        <button class="btn btnPrimary">button</button>
+    </div>
+    
+</template>
