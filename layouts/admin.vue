89dca9a050a6f95351589b779228316cc7b99ae5
ageevweb <ageevweb@gmail.com>
2020-04-27 15:41:49 +0300

    comment table with static comments

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -3,8 +3,10 @@
     Header
     .wrapper-content.wrapper-content--fixed
       Intro(title="Admin Page")
-        nuxt-link.link.linkWhite(to="/admin") toAdmin
-        nuxt-link.link.linkWhite(to="admin/new-post") toNewPost
+        nuxt-link.link.linkWhite(to="/admin") Admin
+        nuxt-link.link.linkWhite(to="admin/new-post") NewPost
+        nuxt-link.link.linkWhite(to="admin/comments") Comments
+
       nuxt
 </template>
 
---
+++ pages/admin/comments/index.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  commentTable( :thead="['Name', 'Text', 'Status', 'Change status', 'Delete']")
+    tbody(slot="tbody")
+      tr(v-for="comment in comments" :key="comment.id")
+        td
+          span {{ comment.name }}
+        td
+          span {{ comment.text }}
+        td
+          span.status.true(v-if="comment.status") publish
+          span.status.false(v-else) hidden
+        td
+          span.link(@click="changeComment(comment.id)") Change status
+        td  
+          span.link(@click="deleteComment(comment.id)") Delete
+
+</template>
+
+<script>
+import commentTable from '@/components/admin/CommentTable'
+
+export default {
+  layout: 'admin',
+
+  data(){
+    return {
+      comments: [
+        {id: 1, name: 'Alex', text: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', status: true},
+        {id: 2, name: 'Bishop', text: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', status: false},
+      ]
+    }
+  },
+
+  components: {
+    commentTable
+  },
+
+  methods: {
+    changeComment(id){
+      console.log(id)
+    },
+
+    deleteComment(id){
+      console.log(id)
+    }
+  }
+}
+</script>
